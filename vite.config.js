ea64953aee3eebbb6275423f1316a3162b443b6c
thibeauu <thibeaunauwelaerts@gmail.com>
2025-04-16 18:18:14 +0200

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ITalentPortfolio/', // Wijzig naar je repository naam
+  base: '/italent-portfolio/', // Aangepast naar de juiste URL (let op: lowercase)
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

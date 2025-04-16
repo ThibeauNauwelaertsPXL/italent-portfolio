210569ab8ba08fe56157bb1da3851bd4133c502e
thibeauu <thibeaunauwelaerts@gmail.com>
2025-04-16 18:12:14 +0200

    pls fix

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/ITalentPortfolio/', // Wijzig naar je repository naam
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

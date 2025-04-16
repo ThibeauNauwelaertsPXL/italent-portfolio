dcb15313919041e33f743956a18d6f4e6d88ad5c
thibeauu <thibeaunauwelaerts@gmail.com>
2025-04-16 18:34:54 +0200

    dit werkt sws niet

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://thibeaunauwelaertspxl.github.io/italent-portfolio/', // Aangepast naar de juiste URL (let op: lowercase)
+  base: '/italent-portfolio/', // Changed to relative path format for GitHub Pages
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

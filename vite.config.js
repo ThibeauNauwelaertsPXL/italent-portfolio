60e46757c8ec2b0bdab2d8df94f84eda28ff116b
thibeauu <thibeaunauwelaerts@gmail.com>
2025-04-16 18:03:52 +0200

    fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/ITalentPortfolio/'
+  base: '/ITalentPortfolio/',
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+      'vue': 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+    }
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'vendor': ['vue', 'vue-router']
+        }
+      }
+    }
+  }
 })

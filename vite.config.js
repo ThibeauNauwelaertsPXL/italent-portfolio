2c5e30c31edc94663d80b9037ad2c681149af944
thibeauu <thibeaunauwelaerts@gmail.com>
2025-04-19 16:14:58 +0200

    help

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 export default defineConfig({
   plugins: [vue()],
-  base: './', // Use relative paths instead of absolute paths
+  base: './', 
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
